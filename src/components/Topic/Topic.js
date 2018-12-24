04db1d9b42d741da442ad78c942c818da53419da
Sunny-zz <1348369672@qq.com>
2018-12-24 17:11:30 +0800

    collect

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -15,17 +15,25 @@ class Topic extends Component {
   }
   getTopic = () => {
     const { id } = this.props.match.params
-    axios.get(`https://cnodejs.org/api/v1/topic/${id}`).then(res => {
-      console.log(res.data.data)
-      //  将该对象下的 replies 数组内的每一项添加一条属性  isShowArea 来控制该评论下的 文本输入框是否展示
-      res.data.data.replies = res.data.data.replies.map(e => {
-        e.isShowArea = false
-        return e
+    const { token } = sessionStorage
+
+    axios
+      .get(
+        `https://cnodejs.org/api/v1/topic/${id}?${
+          token ? `accesstoken=${token}` : ''
+        }`
+      )
+      .then(res => {
+        console.log(res.data.data)
+        //  将该对象下的 replies 数组内的每一项添加一条属性  isShowArea 来控制该评论下的 文本输入框是否展示
+        res.data.data.replies = res.data.data.replies.map(e => {
+          e.isShowArea = false
+          return e
+        })
+        this.setState({
+          topic: res.data.data
+        })
       })
-      this.setState({
-        topic: res.data.data
-      })
-    })
   }
   render() {
     const { topic, comment, otherComment } = this.state
@@ -37,6 +45,17 @@ class Topic extends Component {
         <span>
           ·作者{topic.author.loginname} ·来自{filter(topic.tab)}
         </span>
+        {token ? (
+          <button
+            onClick={() => {
+              this.collect(topic.id)
+            }}
+          >
+            {topic.is_collect ? '取消收藏' : '收藏'}
+          </button>
+        ) : (
+          ''
+        )}
         <div
           className='content'
           dangerouslySetInnerHTML={{ __html: topic.content }}
@@ -182,14 +201,36 @@ class Topic extends Component {
         const newTopic = { ...topic }
         // 一下操作是更新本地的点赞  也就是更新了 topic.replies 下面的某条评论下的 ups 数组
         // 当返回的 action 是 up 的时候，也就说明原来没点赞，点了之后就要讲你的 id 添加到 ups 内，反之删除，在这里为什么不使用 axios 请求重新获取数据，因为可以直接对 state 进行修改，就完全和网上的数据一致了，所以不发请求更新。
+        // 判断是否点过赞 可以使用 res.data.action  也可以使用 获取主题详情的时候添加参数 token，返回的数据中 评论数组内会有 isuped 属性
         if (res.data.action === 'up') {
           newTopic.replies.find(e => e.id === id).ups.push(userId)
         } else {
           newTopic.replies.find(e => e.id === id).ups = newTopic.replies
             .find(e => e.id === id)
             .ups.filter(e => e !== userId)
-          console.log(newTopic)
         }
+        this.setState({
+          topic: newTopic
+        })
+      })
+  }
+  collect = id => {
+    const { token } = sessionStorage
+    const { topic } = this.state
+    const isCollect = topic.is_collect
+    axios
+      .post(
+        `https://cnodejs.org/api/v1/topic_collect/${
+          isCollect ? 'de_collect' : 'collect'
+        }`,
+        {
+          accesstoken: token,
+          topic_id: id
+        }
+      )
+      .then(() => {
+        const newTopic = { ...topic }
+        newTopic.is_collect = !newTopic.is_collect
         this.setState({
           topic: newTopic
         })
